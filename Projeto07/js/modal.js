4629e3700c6edeabc666ff228c138e38da3e8787
Michael Vinicius <129618003+MichaelV1701@users.noreply.github.com>
2023-12-11 21:11:04 -0300

    Update modal.js

--- Projeto07/js/modal.js
+++ Projeto07/js/modal.js
@@ -4,7 +4,6 @@ export const Modal = {
     btnClose: document.querySelector('.modal button.close'), 
 
     open()  { Modal.wrapper.classList.add('open') }, 
-
     close() { Modal.wrapper.classList.remove('open') } } 
 
 Modal.btnClose.onclick = () => {
